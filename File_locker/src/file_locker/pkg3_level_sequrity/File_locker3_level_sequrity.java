d5cfdba6e20e664fb34b5a4c92245feb6e730e5b
Ashish Jaiswal <imashishjaiwal99@gmail.com>
2019-04-05 05:03:29 +0530

    file locker and hider for multiple user
    It locks file and hide it and it can be opened using login using your Password and Recieved OTP.
    it is developed for multiple users.

---
+++ File_locker/src/file_locker/pkg3_level_sequrity/File_locker3_level_sequrity.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file_locker.pkg3_level_sequrity;
+
+/**
+ *
+ * @author HP
+ */
+public class File_locker3_level_sequrity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        First first=new First();
+first.setVisible(true);// TODO code application logic here
+    }
+    
+}
